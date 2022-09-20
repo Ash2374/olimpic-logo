4dde0bd14065c05e9a692ff6d1b9eafc73f90f1e
Ash2374 <110349011+Ash2374@users.noreply.github.com>
2022-09-19 19:48:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,55 @@
-/*get the canvas element using its id and store it in a variable “canvas” so that we can 
-manipulate the canvas throughout the js code.*/
-canvas=document.getElementById("mycanvas");
-ctx=canvas.getContext("2d");
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
 
-
-//Get rference of canvas created.
-
-
-color = "red"; 
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "grey".
+    Use lineWidth and set the width to 1.
+    Use rect() to create a rectangle at 150 and 143
+with 430 width and 200 height.
+*/
 
 ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
-// Attach canvas and addEventListener with 'mousedown' event.
-canvas.addEventListener("mousedown", my_mousedown);
+/*
+    Use beginPath() function to begin a path.
+    Then, use strokeStyle to set the color to "blue".
+    Use lineWidth and set the width to 5.
+    Use arc() to create a circle at x = 150 and y = 143
+with 40 as radius, 0 startAngle and 2 * Math.PI endAngle.
+*/
 
-
-
-function my_mousedown(e)
-{
-    //taking color from input box
-    //additional activity start
-    color=document.getElementById("color").value;
-    console.log(color);
-
-
-
-    //additional activity ends
-    //Get the x and y coordinates when the mouse is clicked.
-    mouse_x=e.clientX-canvas.offsetLeft;
-    mouse_y=e.clientY-canvas.offsetTop;
-
-        
-    console.log("X = " + mouse_x + " ,Y =  " + mouse_y);
-    //Call the "circle()" function with mouse_x and mouse_y as parameters.  
-    circle(mouse_x , mouse_y);
-
-}
-
-
-function circle(mouse_x , mouse_y)
-{
 ctx.beginPath();
-//Assign 'color' to the 'strokeStyle' of the circle.
-ctx.strokeStyle = color;
-ctx.lineWidth = 2;
-// Use "arc()" function to create a circle.
-ctx.arc(mouse_x , mouse_y, 40 ,0 , 2*Math.PI);
-
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
 ctx.stroke();
-}
 
-//additional activity
-
-
-
-	
+// Similarly, create a black circle with position 350 and 210
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+// Similarly, create a red circle with position 210 and 40
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(450, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+// Similarly, create an orange circle with position 300 and 250
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 5;
+ctx.arc(300, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+// Similarly, create a green circle with position 400 and 250
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
